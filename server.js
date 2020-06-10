a6953cca18ee1d200709ff2d4cd4601f1adb3913
alex-whan <whan.alexander@gmail.com>
2020-06-09 19:54:54 -0700

    Fleshed out Trails constructor functionality

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ const PORT = process.env.PORT || 3001;
 // Tells CORS to let "app" work
 app.use(cors());
 
-//let currentLatLong = [];
-
 // "Location" must happen before Weather/Trails as they rely on its data
 
 // GET LOCATION DATA
@@ -74,29 +72,26 @@ app.get('/weather', (request, response) => {
 
 app.get('/trails', (request, response) => {
   try{
-    let latitude = request.query.lat;
-    let longitude = request.query.lon;
-
-    let url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${process.env.TRAIL_API_KEY}`;
-
+    let lat = request.query.latitude;
+    let long = request.query.longitude;
+    let url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=${process.env.TRAIL_API_KEY}`
+    
     superagent.get(url)
       .then(resultsFromSuperAgent => {
-        let trailArray = resultsFromSuperAgent.body.trails.map(trail => new Trail(trail));
+        let trailArray = resultsFromSuperAgent.body.trails.map(hike => new Trail(hike));
         response.status(200).send(trailArray);
       })
-    } catch(err){
+  } catch(err){
       response.status(500).send(errorMessage_500);
-    }
+  }
 })
-
-
 
 // Constructor function to normalize/re-create our JSON data, and ensure that each object is created according to the same format when server receives external data
 function Location(searchQuery, obj){
   this.search_query = searchQuery;
   this.formatted_query = obj.display_name;
-  this.latitude = obj.lat;
-  this.longitude = obj.lon;
+  this.latitude = obj.latitude;
+  this.longitude = obj.longitude;
 }
 
 // Constructor function to normalize/re-create our JSON data from weather.json - taking in the "description" (forecast) and "valid_date" (date) of each daily weather prediction
@@ -105,17 +100,18 @@ function Weather(obj){
   this.time = obj.valid_date;
 }
 
+// Trail constructor
 function Trail(obj){
   this.name = obj.name;
   this.location = obj.location;
   this.trailLength = obj.length;
   this.stars = obj.stars;
-  this.star_votes = obj.starVotes;
+  this.star_votes = obj.star_votes;
   this.summary = obj.summary;
-  this.trail_url = obj.url;
-  this.conditions = obj.conditionStatus;
-  this.condition_date = obj.conditionDate;
-  //this.condition_time = obj.condition_time;
+  this.trail_url = obj.trail_url;
+  this.conditions = obj.condition_status;
+  this.condition_date = obj.condition_date;
+  this.condition_time = obj.condition_time;
 }
 
 // Catch-all (*) in case the route cannot be found
