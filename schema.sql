80dd5952fee985c6f67921adfa279f316d6bca4d
alex-whan <whan.alexander@gmail.com>
2020-06-10 18:10:05 -0700

    Removed extraneous comments from Schema and JS files

--- schema.sql
+++ schema.sql
@@ -9,11 +9,3 @@ CREATE TABLE location (
 );
 
 INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES ('seattle', 'Seattle, King County, Washington, USA', '47.6038321', '-122.3300624');
-
--- Write an INSERT statement here
--- Insert a city that matches searchquery, formatted_query, lat, long
--- Can do this with a regular search on the front end - you're already getting this with your response object (exactly what you get back from your Constructor when you search for a city - those four values)
--- Don't worry about inserting ID - does this automatically
--- To test: Keep trying to search for a city you know is in the DB and see if it keeps calling to the API (shouldn't be if it's happening correctly)
--- Put a console.log ABOVE and INSIDE your superagent call and see which one gets hit each time
--- As soon as that works, THEN go to the INSERT - but initial check needs to work in the first place
--- server.js
+++ server.js
@@ -41,27 +41,6 @@ client.on('error', err => console.error(err));
 // GET LOCATION DATA
 // Use the "app" variable and .get() method to get/return data along the '/location' route and run it through the constructor function to normalize it
 app.get('/location', (request, response) => {
-
-  // let sqlQuery = 'INSERT INTO location (search_query, formatted_query, latitude, longitude;';
-
-  
-
-  // Need 2 things for a client query with PG library - can't just pass one thing
-
-
-
-  // This is where we need to declare what our SQL statement is going to equal
-  // Need to also declare what our safeValue is
-  // Then going to make our first client.query
-
-
-  // Right after the query, chain a .then() to it and inside of that .then() is where we'll have our if/else check
-  // Cannot just do an IF check - has to be conditional. If it's NOT true, need to perform something else very specific.
-  // If check itself will just say: "If this came back from my query, then I want to return that entire row of info"
-  // Else statement: Put your WHOLE superagent call inside of this (city variable, url, superagent, etc - all of it)
-
-
-  // try{
     let city = request.query.city;
     console.log(city);
     let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
@@ -89,20 +68,6 @@ app.get('/location', (request, response) => {
           }
         }).catch(err => console.error(err));
   });
-
-        // BEFORE our response: We're already inside this superagent call because we didn't already have it in our DB
-        // This means we want to do another client query before responding to insert our information into the DB
-        // Essentially: Splitting these things into three functions:
-          // Check DB function
-          // Save to DB function
-          // Get my location function
-          // That could be a full refactor - but you can write them all as one big function for now to get them to work
-
-  // Error message in case there's an error with the server/API call
-//   } catch(err){
-//     response.status(500).send(errorMessage_500);
-//   }
-// })
 
 // GET WEATHER DATA
 // Use the "app" variable and .get() method to get/return data along the '/location' route and run it through the constructor function to normalize it - using a forEach loop
